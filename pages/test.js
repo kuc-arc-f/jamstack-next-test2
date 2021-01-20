71b5f9a35f9c70800438e8c1ab8a936a242c14f0
中島浩二 <knaka0209@gmail.com>
2021-01-20 17:16:15 +0900

    up,0120a4

---
+++ pages/test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import LibCommon from '../libs/LibCommon'
+import LibPagenate from '../libs/LibPagenate'
+import TopHeadBox from '../components/TopHeadBox'
+import IndexRow from './IndexRow';
+import PagesRow from './PagesRow';
+//
+function Page(data) {
+console.log(data.blogs)
+  var items = data.blogs.contents
+  return (
+    <Layout>
+      <div className="body_main_wrap">
+        <div className="container">test:
+        <ul>
+        {items.map((item, index) => {
+          return (<IndexRow key={index}
+                  id={item.id} title={item.title} />       
+          )
+        })}      
+        </ul>
+        </div>
+      </div>
+    </Layout>
+    )
+}
+export const getStaticProps = async context => {
+console.log( process.env.API_KEY )
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const res = await fetch(
+    `https://jamstack-test-kuc.microcms.io/api/v1/blog1`,
+    key,
+  );
+  const blogs = await res.json();
+  console.log(blogs)
+  return {
+    props : {
+      blogs: blogs,
+    }
+  };
+}
+export default Page
